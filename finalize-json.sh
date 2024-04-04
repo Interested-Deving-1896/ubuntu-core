18497ffda71ec3542224fead6c11bce3e8d88bb4
Kevin Ottens <kevin.ottens@enioka.com>
2024-04-04 16:07:02 +0200

    Use canonical as authority-id if the account is not verified

--- finalize-json.sh
+++ finalize-json.sh
@@ -5,9 +5,16 @@ INPUT=$2
 OUTPUT=$3
 
 UBUNTU_ID=`snapcraft whoami | grep '^id:' | awk '{print $2;}'`
+ACCOUNT_VALIDATION=`snap known --remote account account-id=$UBUNTU_ID | grep '^validation:' | awk '{print $2;}'`
 TIMESTAMP=`date -Iseconds --utc`
+
+AUTHORITY_ID="canonical"
+if [ $ACCOUNT_VALIDATION = "verified" ]; then
+  AUTHORITY_ID=$UBUNTU_ID
+fi
+
 JQ_SCRIPT="
-  .[\"authority-id\"] = \"$UBUNTU_ID\" | 
+  .[\"authority-id\"] = \"$AUTHORITY_ID\" |
   .[\"brand-id\"] = \"$UBUNTU_ID\" |
   .[\"grade\"] = \"$GRADE\" |
   .[\"display-name\"] = (.[\"display-name\"] + \", $GRADE\") |
